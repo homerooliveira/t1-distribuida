cd42ad157e782736776ff6cefb4630539cf1a6a6
Eduardo Segura Fornari <eduardo.fornari@icloud.com>
2019-04-13 17:51:50 -0300

    new thread

--- src/com/pucrs/distribuida/MulticastClientMetal.java
+++ src/com/pucrs/distribuida/MulticastClientMetal.java
@@ -7,14 +7,11 @@ import java.util.*;
 public class MulticastClientMetal {
 
     public static void main(String[] args) throws IOException {
-        byte[] saida = new byte[256];
-        String mens = "Alô, Homero!";
-        saida = mens.getBytes();
-        DatagramSocket socket = new DatagramSocket();
+
         InetAddress grupo = InetAddress.getByName("230.0.0.1");
+        byte[] saida =  "Alô, Homero!".getBytes();
         DatagramPacket pacote = new DatagramPacket(saida,saida.length,grupo,5000);
-        socket.send(pacote);
-        socket.close();
+
     }
 
 }
--- src/com/pucrs/distribuida/MulticastServerMetal.java
+++ src/com/pucrs/distribuida/MulticastServerMetal.java
@@ -1,23 +1,48 @@
 package com.pucrs.distribuida;
 
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.InetAddress;
-import java.net.MulticastSocket;
+import java.net.*;
+import java.util.Scanner;
 
 public class MulticastServerMetal {
     public static void main(String[] args) throws IOException {
-        MulticastSocket socket = new MulticastSocket(5000);
-        InetAddress grupo = InetAddress.getByName("230.0.0.1");
-        socket.joinGroup(grupo);
-        while (true) {
-            byte[] entrada = new byte[256];
-            DatagramPacket pacote = new DatagramPacket(entrada, entrada.length);
-            socket.receive(pacote);
-            String recebido = new String(pacote.getData(), 0, pacote.getLength());
-            System.out.println("Received: " + recebido);
-//            socket.leaveGroup(grupo);
-//            socket.close();
-        }
+        new Thread(() -> {
+            MulticastSocket socket = null;
+            try {
+                socket = new MulticastSocket(5000);
+                InetAddress grupo = InetAddress.getByName("230.0.0.1");
+                socket.joinGroup(grupo);
+                while (true) {
+                    byte[] entrada = new byte[256];
+                    DatagramPacket pacote = new DatagramPacket(entrada, entrada.length);
+                    socket.receive(pacote);
+                    String recebido = new String(pacote.getData(), 0, pacote.getLength());
+                    System.out.println("Received: " + recebido);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+
+        new Thread(() -> {
+            Scanner input = new Scanner(System.in);
+            boolean hasWork = true;
+
+            while (hasWork) {
+                byte[] saida = input.nextLine().getBytes();
+                DatagramSocket socket = null;
+                try {
+                    socket = new DatagramSocket();
+                    InetAddress grupo = InetAddress.getByName("230.0.0.1");
+                    DatagramPacket pacote = new DatagramPacket(saida,saida.length,grupo,5000);
+                    socket.send(pacote);
+                    socket.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }).start();
     }
 }
