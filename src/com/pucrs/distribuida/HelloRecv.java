aacda7263cdc4466d29e0f9e1c3c4583eee7cd66
Homero Oliveira <homero.oliveira@ilegra.com>
2019-04-13 15:21:17 -0300

    testing multicast

--- src/com/pucrs/distribuida/HelloRecv.java
+++ src/com/pucrs/distribuida/HelloRecv.java
@@ -4,7 +4,8 @@ import java.net.*;
 import java.io.IOException;
 
 public class HelloRecv {
-        public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) {
 
             new Thread(() -> {
                 MulticastSocket socket = null;
@@ -12,33 +13,37 @@ public class HelloRecv {
                     socket = new MulticastSocket(5000);
                     InetAddress grupo = InetAddress.getByName("230.0.0.1");
                     socket.joinGroup(grupo);
-                    byte[] entrada = new byte[256];
-                    DatagramPacket pacote = new DatagramPacket(entrada,entrada.length);
-                    socket.receive(pacote);
-                    String recebido = new String(pacote.getData(),0,pacote.getLength());
-                    System.out.println("Received: "+recebido);
-                    socket.leaveGroup(grupo);
-                    socket.close();
+                    while(true) {
+                        System.out.println("receive");
+                        byte[] entrada = new byte[256];
+                        DatagramPacket pacote = new DatagramPacket(entrada,entrada.length);
+                        socket.receive(pacote);
+                        String recebido = new String(pacote.getData(),0, pacote.getLength());
+                        System.out.println("Received: "+recebido);
+                    }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }).run();
 
 
-            new Thread(() -> {
-                String mens = "Alô, mundo!";
-                byte[] saida = mens.getBytes();
-                DatagramSocket socket = null;
+        new Thread(() -> {
 
-                try {
-                    socket = new DatagramSocket();
-                    InetAddress grupo = InetAddress.getByName("230.0.0.1");
-                    DatagramPacket pacote = new DatagramPacket(saida,saida.length,grupo,5000);
+            String mens = "Alô, mundo!";
+            byte[] saida = mens.getBytes();
+            DatagramSocket socket = null;
+
+            try {
+                System.out.println("send");
+                socket = new DatagramSocket();
+                InetAddress grupo = InetAddress.getByName("230.0.0.1");
+                while (true) {
+                    DatagramPacket pacote = new DatagramPacket(saida, saida.length, grupo, 5000);
                     socket.send(pacote);
-                    socket.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
                 }
-            }).run();
-        }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).run();
+    }
 }
